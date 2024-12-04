81fc9d0d1e809c5149906499c3e68b196ce45125
isbest <icoderdev@outlook.com>
2024-12-04 13:24:00 +0800

    remove debug js

--- kubejs/server_scripts/src/recipes.js
+++ kubejs/server_scripts/src/recipes.js
@@ -1,8 +1,8 @@
 ServerEvents.recipes(event => {
-    event.forEachRecipe({ type:'tfcorewashing:sluice' }, recipe => {
-        if (recipe.getId().startsWith('tfcorewashing')) {
-            // tfc_ie_addon:arc_furnace/tetrahedrite
-            JsonIO.write("./recipes/" + String(recipe.getId()).replace(/:|\//g, '_') + ".json", recipe.json)
-        }
-    })
+    // event.forEachRecipe({ type:'tfcorewashing:sluice' }, recipe => {
+    //     if (recipe.getId().startsWith('tfcorewashing')) {
+    //         // tfc_ie_addon:arc_furnace/tetrahedrite
+    //         JsonIO.write("./recipes/" + String(recipe.getId()).replace(/:|\//g, '_') + ".json", recipe.json)
+    //     }
+    // })
 })
